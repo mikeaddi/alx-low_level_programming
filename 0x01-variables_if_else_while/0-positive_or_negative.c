e88c04a462206a5fe36173d8516644aaad2e40e3
alx low level <addi.m13@outlook.com>
2023-03-17 15:17:41 -0700

    Positive anything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,8 +20,8 @@ int main(void)
 		printf("%d is positive", n);
 	else if (n == 0)
 		printf("%d is zero", n);
-	else if (n < 0)
-		printf("%d is negetive", n);
+	else
+		printf("%d is negative", n);
 	printf("\n");
 	return (0);
 }
