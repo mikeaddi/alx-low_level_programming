43b185a7e205db9a4c02face45fc09dc80bb9384
alx low level <addi.m13@outlook.com>
2023-03-17 08:02:30 -0700

    UNIX operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - C program that prints line
+ * Return: always 1 on success
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
