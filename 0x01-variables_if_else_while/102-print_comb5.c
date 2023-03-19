6efa38f218d858425baa16958fd17db96cb6d538
alx low level <addi.m13@outlook.com>
2023-03-19 12:30:10 -0700

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main functin
+ * Description: "print to digit numbers"
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = 0; b <= 99; b++)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
