b21615dd80aca752320b493725c515f26da479b6
alx low level <addi.m13@outlook.com>
2023-03-23 10:47:52 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,7 +16,7 @@ int main(void)
 	unsigned long int l = 1000000000;
 	unsigned long int bef1;
 	unsigned long int bef2;
-	unsigned long int afti;
+	unsigned long int aft1;
 	unsigned long int aft2;
 
 	printf("%lu", bef);
@@ -30,15 +30,15 @@ int main(void)
 
 	bef1 = (bef / l);
 	bef2 = (bef % l);
-	aft2 = (aft / l);
+	aft1 = (aft / l);
 	aft2 = (aft % l);
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", aft2 + (aft2 / l));
+		printf(", %lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
-		aft2 = aft2 + bef1;
-		bef1 = aft2 - bef1;
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
 		bef2 = aft2 - bef2;
 	}
