e1adbe8892c160ec9d10996508e71034242b5cdc
alx low level <addi.m13@outlook.com>
2023-03-28 08:03:37 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the string lenght
+ * @s: string whose lenght to be printed
+ * Return: lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
