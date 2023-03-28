de7dac06742da3ac64027ed610d9bc24ba203d54
alx low level <addi.m13@outlook.com>
2023-03-28 09:06:25 -0700

    Leap year

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -3,7 +3,7 @@
 
 /**
 * main - takes a date and prints how many days are left in the year, taking
-* leap years into account,convert_day
+* leap years into account
 * Return: 0
 */
 
@@ -18,7 +18,7 @@ int main(void)
 	year = 1997;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-	
+
 	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
