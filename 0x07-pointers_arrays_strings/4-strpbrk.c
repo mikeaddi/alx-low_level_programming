2f75a6c564d3e74fefa7f20363cacc28f46f31fc
alx low level <addi.m13@outlook.com>
2023-04-03 11:56:47 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - point of entry
+ * @s: input
+ * @accept: input
+ * Return: Always 0 upon sucess
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int m;
+
+	while (*s)
+	{
+		for (m = 0; accept[m]; m++)
+		{
+		if (*s == accept[m])
+		return (s);
+	s++;	}
+	}
+
+return ('\0');
+}
