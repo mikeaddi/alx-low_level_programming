c743f85f67e13b5b8388864723b6127216609171
alx low level <addi.m13@outlook.com>
2023-04-19 16:05:03 -0700

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 upon success
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, l;
+	char *arry;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arry = (char *)main;
+
+	for (l = 0; l < bytes; l++)
+	{
+		if (l == bytes - 1)
+		{
+			printf("%02hhx\n", arry[1]);
+			break;
+		}
+		printf("%02hhx ", arry[l]);
+	}
+	return (0);
+}
