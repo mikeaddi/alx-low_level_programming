157e2778f8347f2280943d086d57a46f7963e806
Michael Addison <addi.m13@outlook.com>
2023-05-02 08:41:12 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint2 -  function that frees a listint_t list.
+ * @head: points to the first node in the link
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
